9a785e9c7b8ac73b15df3391ea19813a673bde6d
jyothiskariyilroy-arch <jyothiskariyilroy@gmail.com>
2026-04-03 18:55:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["awgaulwkwdopvecmigyc.supabase.co"], // 👈 ADD THIS
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
   },
